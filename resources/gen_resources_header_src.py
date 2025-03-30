cede8369843cbdca816bd7194b89459a81c883c1
Ace_Radom <sichenradomlyu@gmail.com>
2025-03-30 21:05:59 +0200

    resources: do not regenerate when nothing changes

--- resources/gen_resources_header_src.py
+++ resources/gen_resources_header_src.py
@@ -53,11 +53,34 @@ def main():
     if not os.path.exists(source_output_dir):
         os.makedirs(source_output_dir)
 
-    with open(os.path.join(header_output_dir, "resources.h"), 'w', encoding='utf-8') as wfile:
-        wfile.write(header)
+    header_output_path = os.path.join(header_output_dir, "resources.h")
+    header_need_to_generate = True
+    source_output_path = os.path.join(source_output_dir, "resources.cpp")
+    source_need_to_generate = True
 
-    with open(os.path.join(source_output_dir, "resources.cpp"), 'w', encoding='utf-8') as wfile:
-        wfile.write(source)
+    if os.path.exists(header_output_path):
+        with open(header_output_path, 'r', encoding='utf-8') as rfile:
+            old_header = rfile.read()
+        if old_header == header:
+            header_need_to_generate = False
+    
+    if os.path.exists(source_output_path):
+        with open(source_output_path, 'r', encoding='utf-8') as rfile:
+            old_source = rfile.read()
+        if old_source == source:
+            source_need_to_generate = False
+
+    if header_need_to_generate:
+        with open(os.path.join(header_output_dir, "resources.h"), 'w', encoding='utf-8') as wfile:
+            wfile.write(header)
+    else:
+        print("-- resources/resources.h is up to date")
+
+    if source_need_to_generate:
+        with open(os.path.join(source_output_dir, "resources.cpp"), 'w', encoding='utf-8') as wfile:
+            wfile.write(source)
+    else:
+        print("-- resources.cpp is up to date")
 
 if __name__ == "__main__":
     main()
